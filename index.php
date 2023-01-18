9e8029d4293ac3956a529ead2cccc686bb8e922a
Cyprian11 <barabaszcyprianti@gmail.com>
2023-01-18 09:08:49 +0100

    Zadanie wypozyczalnia

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <center>
+        <div class="zao-zielone">
+            <h1>😀Witaj w wypożczalni filmów!😀</h1><br>
+            <h1>Welcome in Witkowo</h1>
+
+</div>
+        <h1>LOGOWANIE</h1>
+            <form method="POST">
+            Login: <input type="text" name="login"><br>
+            Haslo: <input type="password" name="password"><br>
+            <input type="submit" name="submit">
+            </center>
+            <center>
+            <div class="zao-rozowe">
+            <b><p><a href="rejestracja.php" target="_blank">REJESTRACJA<br><u>Kliknij aby się zerejestrować</u></a></p><b>
+</center>
+</div>
+</div>
+</body>
+
+
+
+
+    </html>
